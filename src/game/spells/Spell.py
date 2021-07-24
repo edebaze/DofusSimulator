3235d4ec810970ef7e7e2802a7cb7b297ae2ea09
edebaze <e.debaze@gmail.com>
2021-07-24 21:27:28 +0200

    add new features to spells

--- src/game/spells/Spell.py
+++ src/game/spells/Spell.py
@@ -1,22 +1,65 @@
 from globals.path import IMAGE_DIR
+
+import numpy as np
 import random
 
 
 class Spell:
     IMG_DIR = IMAGE_DIR.replace('\\', '/') + '/spells'
 
+    ELEMENT_NEUTRAL = 'neutral'
+    ELEMENT_FIRE = 'fire'
+    ELEMENT_AIR = 'air'
+    ELEMENT_WATER = 'water'
+    ELEMENT_EARTH = 'earth'
+
+    ZONE_NONE = 0
+    ZONE_ITEM = 1
+    ZONE_CENTER = 2
+
     def __init__(self):
         self.name: str = ''
-        self.type: int = 0
+        self.id: int = 0
         self.img: str = ''
 
+        # PA/PO
         self.pa: int = 0
-        self.po: int = 1
+        self.cooldown: int = 0
+        self.min_po: int = 0
+        self.max_po: int = 1
+        self.is_po_mutable: bool = True
+
+        # DAMAGES
+        self.elem: str = self.ELEMENT_NEUTRAL
         self.max_damage: int = 0
         self.min_damage: int = 0
 
-        self.is_po_mutable: bool = True
+        # EFFECTS
+        self.pa_retreat: int = 0
+        self.pm_retreat: int = 0
+        self.po_retreat: int = 0
+        self.bump: int = 0
+
+        # SPELL TARGET
+        self.is_line: bool = False
+        self.spell_target: np.ndarray = np.asarray([[2]])
 
     def damages(self) -> int:
         # return random.randint(self.min_damage, self.max_damage)
         return self.max_damage
+
+    @staticmethod
+    def create_zone(range):
+        zone = np.empty(0)
+        return zone
+
+    @staticmethod
+    def create_zone_cross(range):
+        zone = np.empty(0)
+        return zone
+
+    @staticmethod
+    def create_zone_line(range):
+        zone = np.ones((1, range)) * Spell.ZONE_ITEM
+        zone[0, range-1] = Spell.ZONE_CENTER
+        return zone
