fd6d0058d320598f641e5b1ba5900ccd563bab02
edebaze <e.debaze@gmail.com>
2021-07-28 17:15:30 +0200

    update render script
    - set mem_size to 1 (to preserve RAM and because there is no training)
    - set epsilon to 0 to avoid random_actions
    - remove usless training params structure
    - update input_dim
    - add loaded model

--- src/main.py
+++ src/main.py
@@ -1,30 +1,39 @@
+from globals.path import *
 from game.Engine import Engine
 from game.interface.GUI import GUI
 from agents import Agent
+from tensorflow import keras
 
-MAP_NUMBER = 1
+MAP_NUMBER = 2
 
 if __name__ == '__main__':
     env = Engine(map_number=MAP_NUMBER, flag_create_dir=False)
     state = env.reset()
 
+    model_iop = keras.models.load_model(os.path.join(MODEL_DIR, 'saved', 'iop_2.h5'))
+    model_cra = keras.models.load_model(os.path.join(MODEL_DIR, 'saved', 'cra_2.h5'))
+
     agent1 = Agent(
-        is_activated=False,
-        model_structure=[512, 512, 256, 256],
-        input_dim=state.shape,
+        is_activated=True,
+        mem_size=1,
+        input_dim=[state[0].shape, state[1].shape],
         actions=env.actions,
-        mem_size=1
+        epsilon=0,
+        epsilon_end=0,
+        model=model_iop,
     )
 
     agent2 = Agent(
         is_activated=False,
-        model_structure=[512, 512, 256, 256],
-        input_dim=state.shape,
+        mem_size=1,
+        input_dim=[state[0].shape, state[1].shape],
         actions=env.actions,
-        mem_size=1
+        epsilon=0,
+        epsilon_end=0,
+        model=model_cra,
     )
 
-    env = Engine(map_number=MAP_NUMBER, agents=[agent1, agent2])
+    env = Engine(map_number=MAP_NUMBER, agents=[agent1, agent2], flag_create_dir=False)
     gui = GUI(env)
     gui.reset()
     gui.render()
