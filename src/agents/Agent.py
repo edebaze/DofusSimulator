4622a1100c0db3e272a1c72b224622abf0a1fc73
edebaze <e.debaze@gmail.com>
2021-07-29 11:55:55 +0200

    update choose_random_action

--- src/agents/Agent.py
+++ src/agents/Agent.py
@@ -209,8 +209,12 @@ class Agent:
         """
         # -- choose random action (if random is allowed)
         if np.random.random() < self.epsilon and allow_random:
-            action = np.random.choice(self.actions)
-            while action in self.blocked_actions and len(self.blocked_actions) != self.n_actions:
+            if len(self.blocked_actions) != self.n_actions:
+                actions = copy.copy(self.actions)
+                for blocked_action in self.blocked_actions:
+                    actions.remove(blocked_action)
+                action = np.random.choice(actions)
+            else:
                 action = np.random.choice(self.actions)
 
         # -- choose action from model prediction
